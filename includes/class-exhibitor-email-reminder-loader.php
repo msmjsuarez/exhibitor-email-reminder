f3efaa906bf28c421421db0959c5469967b22cd5
MJ Suarez Layasan <msmjsuarez@gmail.com>
2024-05-19 22:51:35 +0800

    Update class-exhibitor-email-reminder-loader.php

--- includes/class-exhibitor-email-reminder-loader.php
+++ includes/class-exhibitor-email-reminder-loader.php
@@ -3,7 +3,7 @@
 /**
  * Register all actions and filters for the plugin
  *
- * @link       https://exhibitor-portal.uk
+ * @link       #
  * @since      1.0.0
  *
  * @package    Exhibitor_Email_Reminder
