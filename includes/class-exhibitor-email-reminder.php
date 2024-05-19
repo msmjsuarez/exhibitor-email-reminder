f1f8b3a3ee07cb4658d3201b445a45a9a61871c6
MJ Suarez Layasan <msmjsuarez@gmail.com>
2024-05-19 22:51:57 +0800

    Update class-exhibitor-email-reminder.php

--- includes/class-exhibitor-email-reminder.php
+++ includes/class-exhibitor-email-reminder.php
@@ -6,7 +6,7 @@
  * A class definition that includes attributes and functions used across both the
  * public-facing side of the site and the admin area.
  *
- * @link       https://exhibitor-portal.uk
+ * @link       #
  * @since      1.0.0
  *
  * @package    Exhibitor_Email_Reminder
