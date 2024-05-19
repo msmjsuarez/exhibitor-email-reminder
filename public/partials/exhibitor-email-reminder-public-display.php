e6476c4e350252ccbc174e08daeaf7628c9b5998
MJ Suarez Layasan <msmjsuarez@gmail.com>
2024-05-19 22:54:42 +0800

    Update exhibitor-email-reminder-public-display.php

--- public/partials/exhibitor-email-reminder-public-display.php
+++ public/partials/exhibitor-email-reminder-public-display.php
@@ -5,7 +5,7 @@
  *
  * This file is used to markup the public-facing aspects of the plugin.
  *
- * @link       https://exhibitor-portal.uk
+ * @link       #
  * @since      1.0.0
  *
  * @package    Exhibitor_Email_Reminder
