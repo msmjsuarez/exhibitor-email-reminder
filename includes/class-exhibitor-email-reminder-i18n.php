045201c2e070d60f53e3c5c52780364ce7a6f6dc
MJ Suarez Layasan <msmjsuarez@gmail.com>
2024-05-19 22:51:22 +0800

    Update class-exhibitor-email-reminder-i18n.php

--- includes/class-exhibitor-email-reminder-i18n.php
+++ includes/class-exhibitor-email-reminder-i18n.php
@@ -6,7 +6,7 @@
  * Loads and defines the internationalization files for this plugin
  * so that it is ready for translation.
  *
- * @link       https://exhibitor-portal.uk
+ * @link       #
  * @since      1.0.0
  *
  * @package    Exhibitor_Email_Reminder
